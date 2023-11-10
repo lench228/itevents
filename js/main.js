5c31543cff3af5002493604f0382b519337858f2
lench <leonidgneusev@gamil.com>
2023-11-11 00:20:56 +0500

    Почти готова страница реги(остались микроправки)

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import './hero_rotate.js';
 import{createCard} from './data.js';
 import {renderCards} from './cards.js';
 import './filters.js';
+
 let cards = [];
 for(let i= 0; i < 5; i++){
     cards[i] = createCard(i);
