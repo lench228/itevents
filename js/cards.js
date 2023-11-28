aefd15a5b4d228d7b788c41ecd2c291f958f5b90
lench <leonidgneusev@gamil.com>
2023-11-28 21:41:15 +0500

    Правки карточек

--- js/cards.js
+++ js/cards.js
@@ -1,9 +1,29 @@
 
 const cardFragments = document.createDocumentFragment();
-const cardTemplate = document.querySelector('#card__template').content.querySelector('a');
+const cardTemplate = document.querySelector('#card__template').content.querySelector('div');
+
+const tagsTemplate = document.querySelector('#tags__template').content.querySelector('li');
+const tagsFragment = document.createDocumentFragment();
+const createTags = (currentCard) => {
+    tagsFragment.innerHTML = '';
+    const tags = currentCard.tags; 
+
+   console.log(tags);
+    
+    tags.forEach((tag) => {
+        let tagElement = tagsTemplate.cloneNode(true);
+        tagElement.querySelector('a').textContent = tag;
+        tagsFragment.appendChild(tagElement);
+    });
+    return(tagsFragment);
+    
+};
 
 const createCard  = (card) => {
-    let currentCard = cardTemplate.cloneNode(1);
+    let currentCard = cardTemplate.cloneNode(true);
+    let tagsContainer = currentCard.querySelector('.tags'); 
+    tagsContainer.innerHTML = ''; 
+
     currentCard.querySelector('.card__title').textContent = card.title;
     currentCard.querySelector('.card__number').textContent = `0${card.id}`;
     currentCard.querySelector('.card__image').src= card.url;
@@ -11,6 +31,7 @@ const createCard  = (card) => {
     currentCard.querySelector('.card__description').textContent = card.description;
     currentCard.querySelector('.card__date').textContent = card.date;
 
+    tagsContainer.append(createTags(card));
     cardFragments.append(currentCard);
 }
 
