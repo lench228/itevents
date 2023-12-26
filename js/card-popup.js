8579c05092a81fd2bf9a329dfd324c828965dc88
Vladislav <VladislavAndreev1337@yandex.ru>
2023-12-26 16:44:19 +0500

    few changes

--- js/card-popup.js
+++ js/card-popup.js
@@ -12,11 +12,14 @@ const popupAdres = popupCard.querySelector('.open-card-adres');
 const popupOrg = popupCard.querySelector('.open-card-org');
 const popupImg = popupCard.querySelector('.open-card__image')
 
+const participateButton = document.getElementById('button__participate');
+
 const popupCloseBtn = popupCard.querySelector('.close-modal');
 
 const closePopup = () => {
     body.classList.remove('modal-open');
     popupCard.classList.add('hidden');
+    popupCard.dataset.id = '';
     document.removeEventListener('keydown', onDocumentKeyDown);
     popupCloseBtn.removeEventListener('click', onCloseBtnClick);
 
@@ -34,12 +37,39 @@ const onCloseBtnClick = (evt) =>{
 }
 
 const renderPopup = (card) => {
+    popupCard.dataset.id = card.id;
     popupDescription.textContent = card.description;
     popupTitle.textContent = card.title;
     popupPlace.textContent = card.format;
-    popupAdres.textContent = card.adres;
+    popupAdres.textContent = card.address;
     popupOrg.textContent = card.org;
 
+    
+    if (localStorage.getItem('likedEvents')) {
+        const storagedEvents = JSON.parse(localStorage.getItem('likedEvents'));
+        const index = card.id - 1; // Предполагая, что card.id - 1 является корректным индексом.
+    
+        if (index >= 0 && index < storagedEvents.length) {
+            const eventId = storagedEvents[index].event_id;
+            console.log(eventId);
+    
+            const foundEvent = storagedEvents.find(event => event.event_id === eventId);
+            if (foundEvent) {  
+                console.log(1111) 
+                participateButton.innerHTML = `Не пойду
+                <svg xmlns="http://www.w3.org/2000/svg" width="55" height="30" viewBox="0 0 55 30" fill="none">
+                        <path d="M53.6929 16.3708C54.474 15.5897 54.474 14.3234 53.6929 13.5423L40.965 0.814407C40.1839 0.0333586 38.9176 0.0333586 38.1366 0.814407C37.3555 1.59546 37.3555 2.86179 38.1366 3.64283L49.4503 14.9565L38.1366 26.2703C37.3555 27.0513 37.3555 28.3176 38.1366 29.0987C38.9176 29.8797 40.1839 29.8797 40.965 29.0987L53.6929 16.3708ZM0.256836 16.9565L52.2787 16.9565V12.9565L0.256836 12.9565L0.256836 16.9565Z" fill="white"/>
+                      </svg>`;
+            } 
+        } 
+        else {
+            participateButton.innerHTML = `Пойду
+            <svg xmlns="http://www.w3.org/2000/svg" width="55" height="30" viewBox="0 0 55 30" fill="none">
+                    <path d="M53.6929 16.3708C54.474 15.5897 54.474 14.3234 53.6929 13.5423L40.965 0.814407C40.1839 0.0333586 38.9176 0.0333586 38.1366 0.814407C37.3555 1.59546 37.3555 2.86179 38.1366 3.64283L49.4503 14.9565L38.1366 26.2703C37.3555 27.0513 37.3555 28.3176 38.1366 29.0987C38.9176 29.8797 40.1839 29.8797 40.965 29.0987L53.6929 16.3708ZM0.256836 16.9565L52.2787 16.9565V12.9565L0.256836 12.9565L0.256836 16.9565Z" fill="white"/>
+                  </svg>`;
+        }
+    }
+    
 
     popupImg.src = card.url;
     
--- js/entrance.js
+++ js/entrance.js
@@ -15,3 +15,35 @@ function selectOption(evt) {
     toggleActiveClass(entranceOptions, evt.currentTarget);
     evt.currentTarget.classList.add('entrance__active');
 }
+
+document.querySelector('.modal__register').addEventListener('submit', async (event)  => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    
+    const formObject = {};
+    formData.forEach(function(value, key){
+        value = (value === '') ? 'user' : value;
+        formObject[key] = value;
+    });
+
+    console.log(formObject);
+
+    try {
+        const response = await fetch('http://localhost:3000/test/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formObject)
+        });
+
+        const data = await response.json();
+        console.log(data);
+        window.location.href = "index.html";
+    } catch (err) {
+        console.log(err);
+    }
+
+    //this.validate();
+});
+
